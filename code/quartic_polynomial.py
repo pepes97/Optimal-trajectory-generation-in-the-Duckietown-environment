7c3f92742ea127d8ed5541512a7897586dcfcba5
pepes97 <svevapepe@yahoo.com>
2021-01-20 17:27:52 +0100

    quartic polynomial + quintic polynomial + doc

---
+++ code/quartic_polynomial.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+class QuarticPolynomial:
+
+    def __init__(self, s0, dot_s0, ddot_s0, dot_s1, ddot_s1, T): 
+
+        self.a0 = s0
+        self.a1 = dot_s0
+        self.a2 = ddot_s0 / 2.0
+
+        A = np.array([[3 * T ** 2, 4 * T ** 3],
+                      [6 * T, 12 * T ** 2]])
+
+        b = np.array([dot_s1 - self.a1 - 2 * self.a2 * T,
+                      ddot_s1 - 2 * self.a2])
+        x = np.linalg.solve(A, b)
+
+        self.a3 = x[0]
+        self.a4 = x[1]
+
+    def compute_st(self, t):
+        st = self.a0 + self.a1*t + self.a2*t**2 + self.a3 *t** 3 + self.a4*t**4
+
+        return st
+
+    def compute_first_derivative(self, t):
+        dot_st = self.a1 + 2*self.a2*t + 3*self.a3*t**2 + 4*self.a4*t**3
+
+        return dot_st
+
+    def compute_second_derivative(self, t):
+        ddot_st = 2*self.a2 + 6*self.a3*t + 12*self.a4*t**2
+
+        return ddot_st
+
+    def compute_third_derivative(self, t):
+        dddot_st = 6*self.a3 + 24*self.a4*t
+
+        return dddot_st
---
+++ code/quintic_polynomial.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+class QuinticPolynomial:
+
+    def __init__(self, p0,dot_p0,ddot_p0,p1,dot_p1,ddot_p1, T):
+
+      self.a0 = p0
+      self.a1 = dot_p0
+      self.a2 = ddot_p0/2
+
+      A = np.array([[T**3, T**4, T**5],
+                    [3*T**2, 4*T**3, 5*T**4],
+                    [6*T, 12*T**2, 20*T**3]])
+    
+      b =np.array([p1 - self.a0 - self.a2 *T**2,
+                  dot_p1 - self.a1 - 2*self.a2*T,
+                  ddot_p1-2*self.a2])
+      
+      x = np.linalg.solve(A,b)
+
+      self.a3 = x[0]
+      self.a4 = x[1]
+      self.a5 = x[2]
+
+    def compute_pt(self, t):
+      pt = self.a0 + self.a1*t + self.a2*t**2 + self.a3*t**3 + self.a4*t**4 + self.a5*t**5
+      return pt
+
+    def compute_first_derivative(self, t):
+      dot_pt = self.a0 + self.a1*t + self.a2*t** 2 + self.a3*t**3 + self.a4*t**4 + self.a5*t**5
+      return dot_pt
+
+    def compute_second_derivative(self, t):
+      ddot_pt = 2*self.a2 + 6*self.a3*t + 12*self.a4*t**2 + 20*self.a5*t**3
+      return ddot_pt
+
+    def compute_third_derivative(self, t):
+      dddot_pt = 6*self.a3 + 24*self.a4*t + 60*self.a5*t**2
+      return dddot_pt
