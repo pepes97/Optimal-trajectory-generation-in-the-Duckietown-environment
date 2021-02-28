5f2e0f6e92969eee1162a5065b21a965ff847c0c
SimoneRosset <rossetti.1900592@studenti.uniroma1.it>
2021-02-28 19:17:47 +0100

    odometry with exact integration

--- lib/platform/unicycle.py
+++ lib/platform/unicycle.py
@@ -18,14 +18,25 @@ class Unicycle:
         u[1] : Radial velocity control
         dt   : Delta of time for which control is applied (Default to 10 ms)
         """
+        def S(d_theta):
+            return 1 - 1/6 * d_theta**2 + 1/120 * d_theta**4
+        
+        def C(d_theta):
+            return 1/2 * d_theta - 1/24 * d_theta**3 + 1/720 * d_theta**5
+        
         def kinematicModel():
             t = self.p[2]
             v = u[0]
             w = u[1]
-            return np.array([v * np.cos(t), v * np.sin(t), w])
+            d_rho = dt * v
+            d_theta = dt * w
+            dx = d_rho*S(d_theta)
+            dy = d_rho*C(d_theta)
+            return np.array([dx,dy,d_theta])/dt
 
         assert u.shape == (2,)
         vel = kinematicModel()
+        
         self.p += dt * vel
         return self.p, vel
 
